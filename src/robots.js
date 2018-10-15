dae73e55ed605351616a08ca6c8c2b743230d46a
patmandu <patmandu@github.com>
2018-10-15 21:26:51 +1300

    Created cards and robots.js file

---
+++ src/robots.js
@@ -0,0 +1,98 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Julie Hayes',
+        username: 'baby',
+        email: 'Julie@amo.org'
+    },
+    {
+        id: 2,
+        name: 'Patrick Hayes',
+        username: 'pat',
+        email: 'Pat@hayes.org'
+    },
+    {
+        id: 3,
+        name: 'Esther Hayes',
+        username: 'Mumfy',
+        email: 'mumfy@hayes.org'
+    },
+    {
+        id: 4,
+        name: 'Althea Ebdane',
+        username: 'Ebby',
+        email: 'Althea@ebby.org'
+    },
+    {
+        id: 5,
+        name: 'Azzy Hayes',
+        username: 'Azzy',
+        email: 'Azzy@bear.org'
+    },
+    {
+        id: 6,
+        name: 'Joshi Potter',
+        username: 'Josh',
+        email: 'Josh@potter.org'
+    },
+    {
+        id: 7,
+        name: 'Isaac Hayes',
+        username: 'BigBlackDude',
+        email: 'Isaac@hayes.org'
+    },
+    {
+        id: 8,
+        name: 'Rachel Hayes',
+        username: 'Chinski',
+        email: 'Chinksi@bobs.org'
+    },
+    {
+        id: 9,
+        name: 'Jasmine Hayes',
+        username: 'Jazzy',
+        email: 'Jazzy@hayes.org'
+    },
+    {
+        id: 10,
+        name: 'Eric Hayes',
+        username: 'Rissy',
+        email: 'Rissy@hayes.org'
+    },
+    {
+        id: 11,
+        name: 'Marvel',
+        username: 'CaptainMarvel',
+        email: 'Captain@marvel.org'
+    },
+    {
+        id: 12,
+        name: 'Kiel Ebdane',
+        username: 'Kiel',
+        email: 'Kiel@ebby.org'
+    },
+    {
+        id: 13,
+        name: 'Dean Hayes',
+        username: 'BigD',
+        email: 'BigD@hayes.org'
+    },
+    {
+        id: 14,
+        name: 'Blair Hayes',
+        username: 'BigB',
+        email: 'Blair@hayes.org'
+    },
+    {
+        id: 15,
+        name: 'Destynee Hayes',
+        username: 'DestyChild',
+        email: 'Desty@child.org'
+    },
+    {
+        id: 16,
+        name: 'Angel Hayes',
+        username: 'Angel',
+        email: 'Angel@hayes.org'
+    }
+];
