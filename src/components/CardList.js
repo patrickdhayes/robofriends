e3f97115424f55e3f320bb1b747d9cafd4726ae0
Patrick Hayes <Patrick.Hayes@fisherpaykel.com>
2018-12-21 07:19:32 +1300

    Added Username to carList component

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -12,6 +12,7 @@ const CardList = ({ robots }) => {
                         id={robots[i].id}
                         name={robots[i].name}
                         email={robots[i].email}
+						username={robots[i].username}
                     />
                 );
               })
